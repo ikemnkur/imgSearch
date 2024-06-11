3d57514f8b260371549c7cf699d4d0c904db274a
ikemnkur <68139524+ikemnkur@users.noreply.github.com>
2024-06-11 10:48:21 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Gallery from './Gallery';
 import ImageViewer from './ImageViewer';
 import SearchPage from './SearchPage';
 import UploadImage from './UploadImage';
 import './UploadImage.css';
-import TagManager from 'react-gtm-module'
-const tagManagerArgs = {
-  gtmId: 'G-92GX2GE84L '
-}
-TagManager.initialize(tagManagerArgs)
+// import TagManager from 'react-gtm-module'
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-92GX2GE84L');
+
+// const tagManagerArgs = {
+//   gtmId: 'G-92GX2GE84L '
+// }
+// TagManager.initialize(tagManagerArgs)
 
 function App() {
+  const location = useLocation();
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location]);
   return (
     <Router>
       <Routes>
