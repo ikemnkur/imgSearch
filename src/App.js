bf9bb199d9d57c0840c14a0df2c11cb44e340bee
ikemnkur <68139524+ikemnkur@users.noreply.github.com>
2024-06-11 16:22:05 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Gallery from './Gallery';
 import ImageViewer from './ImageViewer';
