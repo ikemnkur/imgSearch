59ff6ec4cf26314853340ba38a7d940546a35c01
Ikem Nkurumeh <ikemnkur@gmail.com>
2024-06-19 23:21:01 -0500

    added an ad

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -67,7 +67,17 @@ function SearchPage() {
         </div>
         {/* Space for advertisements */}
         <br></br>
-        <div>Advertisement Space</div>
+        <div style={{ margin: 20, padding: 50 }}>
+          Advertisement Space
+          <div dangerouslySetInnerHTML={{
+            __html: `
+                <!-- JuicyAds v3.0 -->
+                <script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+                <ins id="1059978" data-width="300" data-height="112"></ins>
+                <script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':1059978});</script>
+                <!--JuicyAds END-->
+              ` }}></div>
+        </div>
         <br></br>
         <br></br>
         <div>
